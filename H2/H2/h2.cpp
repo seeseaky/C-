b18c42d519aacc5f0d8b0b13619049c558f35b74
seeseaky <seeseaky@gmail.com>
2023-04-16 15:51:18 +0800

    作业2.3

---
+++ H2/H2/h2.cpp
@@ -0,0 +1,7 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main() {
+    printf("     **     \n     **     \n************\n************\n    *  *    \n    *  *    \n");
+        return 0;
+}
---
+++ H3/H3/h3.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main() {
+    float a = 0;
+    scanf("%f", &a);
+    printf("%.3f", a);
+    return 0;
+}
